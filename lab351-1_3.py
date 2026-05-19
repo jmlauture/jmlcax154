7bfdd02f4bd5fe6a96d79b512d2cfbff35b18bb3
jmlauture <pastorlaul@gmail.com>
2026-05-19 14:07:09 -0400

    Staging the LAB351-1 python scripts

---
+++ lab351-1_3.py
@@ -0,0 +1,11 @@
+# Lab 351-1: Introduction to Python Programming lab
+# Author: Jean Lauture
+# To see the outputs of this code, 
+# run it in a Python environment (like VS Code, Jupyter Notebook, PyCharm, 
+# or the command line python3 lab351-1_3.py).                    
+print("Hello World!")
+print("Welcome to Python Programming")
+print("What is your name?") 
+name = input()
+print(f"Hello, {name}! Glad to have you learning Python. Happy coding!")    
+print(f"Goodbye {name}! See you next time.")
